1e460396ad2a8b3b2571e0ed82a34d504095ba33
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-08-28 20:39:23 +0300

    Add Vendor form type

---
+++ src/App/Bundle/CoreBundle/Form/VendorType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class VendorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description', 'textarea')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Bundle\CoreBundle\Entity\Vendor'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_vendor';
+    }
+}
